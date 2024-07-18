a671ca11c54abf3375bc2dcecb40d33b8efa1b21
Sckathach <sckathach@hackademint.org>
2024-07-18 12:47:07 +0200

    Helpers dijkstra

--- lib/chouette.mli
+++
@@ -1,7 +0,0 @@
-open Owl 
-
-module A :
-  sig
-    val empty_square : int -> Arr.arr
-    val closeness : Owl_dense_matrix_d.mat -> Dense.Matrix.Z.mat
-  end
--- lib/helpers.ml
+++ lib/helpers.ml
@@ -1,5 +1,6 @@
 open Owl
 open Printf
+open Chouette
 
 let is_close ?(eps = 0.001) x y = Maths.abs (x -. y) < eps 
 
@@ -11,4 +12,13 @@ let range n =
   let rec aux n l = match n with 
     x -> if x >= 0 then aux (n - 1) (x :: l) else l 
   in aux n []
+
+let print_dijkstra v1 v2 g = 
+  let rec aux = function 
+    [] -> ()
+    | [(x, y)] -> Printf.printf "%d -> %d\n" x y
+    | (x, _) :: q -> Printf.printf "%d -> " x; aux q 
+  in 
+    let p, _ = Dij.shortest_path g v1 v2 in 
+    aux p 
 
